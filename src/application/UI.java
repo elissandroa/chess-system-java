3551485fa6532e90e8508ead4e09a1080cd8233d
Elissandro <elissandroa@hotmail.com>
2020-04-13 20:17:43 -0300

    Special move - En Passant

--- src/application/UI.java
+++ src/application/UI.java
@@ -109,7 +109,7 @@ public class UI {
 	private static void printCapturedPieces(List<ChessPiece> captured) {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-		System.out.println("Captured piedes:");
+		System.out.println("Captured pieces:");
 		System.out.print("White:");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
